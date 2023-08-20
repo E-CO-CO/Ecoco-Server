c37321f9c766c89e3af0f1a0b4a15008e6816f2c
rigizer <rigizer@gmail.com>
2023-08-20 19:44:13 +0900

    [SETTING] Java 17, Spring Boot 3.1.2 기반의 초기 프로젝트 생성

---
+++ src/main/java/com/server/eco_co/EcocoApplication.java
@@ -0,0 +1,13 @@
+package com.server.eco_co;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcocoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcocoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/server/eco_co/EcocoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.eco_co;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcocoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
